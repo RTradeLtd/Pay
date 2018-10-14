dfeecd898a823a7ffb090df38cf187d08d3db50a
potsables <postables@rtradetechnologies.com>
2018-10-14 00:17:54 -0700

    dash: add some print statements

--- dash/client.go
+++ dash/client.go
@@ -2,6 +2,7 @@ package dash
 
 import (
 	"errors"
+	"fmt"
 	"time"
 
 	ch "github.com/RTradeLtd/ChainRider-Go"
@@ -10,7 +11,7 @@ import (
 
 const (
 	devConfirmationCount  = int(3)
-	prodConfirmationCount = int(30)
+	prodConfirmationCount = int(6)
 	dev                   = true
 )
 
@@ -44,6 +45,7 @@ func GenerateDashClient(cfg *config.TemporalConfig) (*DashClient, error) {
 
 // ProcessTransaction is used to process a tx and wait for confirmations
 func (dc *DashClient) ProcessTransaction(txHash string) error {
+	fmt.Println("grabbing transaction")
 	tx, err := dc.C.TransactionByHash(txHash)
 	if err != nil {
 		return err
@@ -52,18 +54,23 @@ func (dc *DashClient) ProcessTransaction(txHash string) error {
 		return errors.New("lock time must be equal to 0")
 	}
 	if tx.Confirmations > dc.ConfirmationCount {
+		fmt.Println("transaction confirmed")
 		return nil
 	}
+	fmt.Println("sleeping for 2 minutes before querying again ")
 	// dash  block time is long, so we can sleep for a bit
 	time.Sleep(time.Minute * 2)
 	for {
+		fmt.Println("grabbing tx")
 		tx, err = dc.C.TransactionByHash(txHash)
 		if err != nil {
 			return err
 		}
 		if tx.Confirmations > dc.ConfirmationCount {
+			fmt.Println("transaction confirmed")
 			return nil
 		}
+		fmt.Println("sleeping for 2 minutes before querying again")
 		time.Sleep(time.Minute * 2)
 	}
 }
