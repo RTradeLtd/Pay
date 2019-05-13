ce50e3b4c53b8e88302cd2a75bd9902c6d879351
Alex <alext@rtradetechnologies.com>
2019-05-13 14:39:41 -0700

    bch: better handling of time sleeps and dev mode
    * Have a package level variable to enable/disable dev mode
    * When processing payment tx, reduce the amount of wait time in dev mode to seconds instead of minutes
    * have a pause function to enable cleaner handling of sleeps

--- bch/bch.go
+++ bch/bch.go
@@ -12,6 +12,7 @@ import (
 var (
 	devConfirmationCount  = 1
 	prodConfirmationCount = 3
+	dev                   = false
 )
 
 // Client is used to interface with the BCH blockchain
@@ -21,7 +22,7 @@ type Client struct {
 }
 
 // NewClient is used to instantiate our new BCH gRPC client
-func NewClient(ctx context.Context, url string, dev bool) (*Client, error) {
+func NewClient(ctx context.Context, url string, devMode bool) (*Client, error) {
 	var opts []grpc.DialOption
 	if dev {
 		opts = append(opts, grpc.WithInsecure())
@@ -31,11 +32,12 @@ func NewClient(ctx context.Context, url string, dev bool) (*Client, error) {
 		return nil, err
 	}
 	var confirmationCount int
-	if dev {
+	if devMode {
 		confirmationCount = devConfirmationCount
 	} else {
 		confirmationCount = prodConfirmationCount
 	}
+	dev = devMode
 	return &Client{pb.NewBchrpcClient(gConn), confirmationCount}, nil
 }
 
@@ -68,7 +70,7 @@ func (c *Client) IsConfirmed(ctx context.Context, tx *pb.GetTransactionResponse)
 		if tx.GetTransaction().GetLockTime() > uint32(height) {
 			return nil
 		}
-		return errors.New("tx is not confirmed")
+		return errors.New("tx is not confirmed, locktime not passed")
 	}
 	return errors.New("tx is not confirmed")
 }
@@ -82,8 +84,8 @@ func (c *Client) ProcessPaymentTx(ctx context.Context, hash string) error {
 	if err := c.IsConfirmed(ctx, tx); err == nil {
 		return nil
 	}
-	// bch has 10 min block time so sleep for 10 min
-	time.Sleep(time.Minute * 10)
+	// wait for some blocks to pass
+	c.pause()
 	for {
 		tx, err = c.GetTx(ctx, hash)
 		if err != nil {
@@ -92,6 +94,14 @@ func (c *Client) ProcessPaymentTx(ctx context.Context, hash string) error {
 		if err := c.IsConfirmed(ctx, tx); err == nil {
 			return nil
 		}
+		c.pause()
+	}
+}
+
+func (c *Client) pause() {
+	if dev {
+		time.Sleep(time.Second * 5)
+	} else {
 		time.Sleep(time.Minute * 10)
 	}
 }
