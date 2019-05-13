12ccd3e19f2575dfc1e0fdf690e225ccfc25e93b
Alex <alext@rtradetechnologies.com>
2019-05-13 14:38:05 -0700

    bch: add test for ProcessPaymentTxt

--- bch/bch_test.go
+++ bch/bch_test.go
@@ -49,13 +49,125 @@ func Test_GetConfirmationCount(t *testing.T) {
 
 func Test_GetCurrentBlockHeight(t *testing.T) {
 	c, fbc := newMockClient()
-	fbc.GetBlockInfoReturnsOnCall(0, &pb.GetBlockInfoResponse{}, nil)
-	if _, err := c.GetCurrentBlockHeight(context.Background()); err != nil {
+	fbc.GetBlockInfoReturnsOnCall(0, &pb.GetBlockInfoResponse{
+		Info: &pb.BlockInfo{Height: 500},
+	}, nil)
+	height, err := c.GetCurrentBlockHeight(context.Background())
+	if err != nil {
+		t.Fatal(err)
+	}
+	if height != 500 {
+		t.Fatal("bad height recovered")
+	}
+}
+
+func Test_IsConfirmed_Success(t *testing.T) {
+	c, fbc := newMockClient()
+	fbc.GetTransactionReturnsOnCall(0, &pb.GetTransactionResponse{
+		Transaction: &pb.Transaction{
+			Confirmations: 6,
+			LockTime:      501,
+		},
+	}, nil)
+	fbc.GetBlockInfoReturnsOnCall(0, &pb.GetBlockInfoResponse{
+		Info: &pb.BlockInfo{Height: 499},
+	}, nil)
+	tx, err := c.GetTx(context.Background(), "hello")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err := c.IsConfirmed(context.Background(), tx); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func Test_IsConfirmed_Fail_Locktime(t *testing.T) {
+	c, fbc := newMockClient()
+	fbc.GetTransactionReturnsOnCall(0, &pb.GetTransactionResponse{
+		Transaction: &pb.Transaction{
+			Confirmations: 6,
+			LockTime:      501,
+		},
+	}, nil)
+	fbc.GetBlockInfoReturnsOnCall(0, &pb.GetBlockInfoResponse{
+		Info: &pb.BlockInfo{Height: 600},
+	}, nil)
+	tx, err := c.GetTx(context.Background(), "hello")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err := c.IsConfirmed(context.Background(), tx); err == nil {
+		t.Fatal("error expected")
+	} else if err.Error() != "tx is not confirmed, locktime not passed" {
+		t.Fatal("wrong error message returned")
+	}
+}
+
+func Test_IsConfirmed_NoConfirmations(t *testing.T) {
+	c, fbc := newMockClient()
+	fbc.GetTransactionReturnsOnCall(0, &pb.GetTransactionResponse{
+		Transaction: &pb.Transaction{
+			Confirmations: 0,
+			LockTime:      501,
+		},
+	}, nil)
+	fbc.GetBlockInfoReturnsOnCall(0, &pb.GetBlockInfoResponse{
+		Info: &pb.BlockInfo{Height: 600},
+	}, nil)
+	tx, err := c.GetTx(context.Background(), "hello")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err := c.IsConfirmed(context.Background(), tx); err == nil {
+		t.Fatal("error expected")
+	} else if err.Error() != "tx is not confirmed" {
+		t.Fatal("wrong error message returned")
+	}
+}
+
+func Test_ProcessPaymentTx(t *testing.T) {
+	c, fbc := newMockClient()
+
+	fbc.GetTransactionReturnsOnCall(0, &pb.GetTransactionResponse{
+		Transaction: &pb.Transaction{
+			Confirmations: 0,
+			LockTime:      501,
+		},
+	}, nil)
+
+	fbc.GetTransactionReturnsOnCall(1, &pb.GetTransactionResponse{
+		Transaction: &pb.Transaction{
+			Confirmations: 1,
+			LockTime:      501,
+		},
+	}, nil)
+
+	fbc.GetTransactionReturnsOnCall(2, &pb.GetTransactionResponse{
+		Transaction: &pb.Transaction{
+			Confirmations: 5,
+			LockTime:      501,
+		},
+	}, nil)
+
+	fbc.GetBlockInfoReturnsOnCall(0, &pb.GetBlockInfoResponse{
+		Info: &pb.BlockInfo{Height: 499},
+	}, nil)
+
+	fbc.GetBlockInfoReturnsOnCall(1, &pb.GetBlockInfoResponse{
+		Info: &pb.BlockInfo{Height: 499},
+	}, nil)
+
+	fbc.GetBlockInfoReturnsOnCall(2, &pb.GetBlockInfoResponse{
+		Info: &pb.BlockInfo{Height: 499},
+	}, nil)
+
+	if err := c.ProcessPaymentTx(context.Background(), "hello"); err != nil {
 		t.Fatal(err)
 	}
 }
 
 func newMockClient() (*Client, *mocks.FakeBchrpcClient) {
+	dev = true
 	fbc := &mocks.FakeBchrpcClient{}
 	c := &Client{}
 	c.confirmationCount = 3
