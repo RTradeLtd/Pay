15af3545c71a370e574bf4bff7b7f20d0ca41996
Alex <alext@rtradetechnologies.com>
2019-05-15 14:32:06 -0700

    queue: remove excessive msg fields

--- queue/payment.go
+++ queue/payment.go
@@ -221,7 +221,7 @@ func (qm *Manager) processBchPaymentConfirmation(ctx context.Context, d amqp.Del
 		d.Ack(false)
 		return
 	}
-	if err := service.BCH.ProcessPaymentTx(ctx, payment.TxHash); err != nil {
+	if err := service.BCH.ProcessPaymentTx(ctx, payment.ChargeAmount, payment.TxHash, payment.DepositAddress); err != nil {
 		logger.Errorw("failed to process payment", "error", err.Error())
 		d.Ack(false)
 		return
--- queue/types.go
+++ queue/types.go
@@ -34,10 +34,8 @@ type DashPaymentConfirmation struct {
 
 // BchPaymentConfirmation is used to confirm a bitcoin cash based payment
 type BchPaymentConfirmation struct {
-	UserName         string `json:"user_name"`
-	SenderAddress    string `json:"sender_address"`
-	RecipientAddress string `json:"recipient_address"`
-	PaymentNumber    int64  `json:"payment_number"`
+	UserName      string `json:"user_name"`
+	PaymentNumber int64  `json:"payment_number"`
 }
 
 // EmailSend is a helper struct used to contained formatted content ot send as an email
