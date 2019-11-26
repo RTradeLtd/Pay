c8054a7d01bf79737065f304a4949462ac3bcf02
Alex Trottier <postables@rtradetechnologies.com>
2019-11-25 18:57:14 -0800

    ethereum: use different test account

--- ethereum/client_test.go
+++ ethereum/client_test.go
@@ -10,7 +10,7 @@ import (
 )
 
 var (
-	key     = `{"address":"7e4a2359c745a982a54653128085eac69e446de1","crypto":{"cipher":"aes-128-ctr","ciphertext":"eea2004c17292a9e94217bf53efbc31ff4ae62f3dd57f0938ab61c949a565dc1","cipherparams":{"iv":"6f6a7a89b556604940ac87ab1e78cfd1"},"kdf":"scrypt","kdfparams":{"dklen":32,"n":262144,"p":1,"r":8,"salt":"8088e943ac0f37c8b4d01592d8bee96468853b6f1f13ca64d201cd68e7dc7b12"},"mac":"f856d734705f35e2acf854a44eb40796518730bd835ecaec01d1f3e7a7037813"},"id":"99e2cd49-4b51-4f01-b34c-aaa0efd332c3","version":3}`
+	key     = `{"address":"cdd092c76eb443cd945d9747a3f960f3e29686fc","crypto":{"cipher":"aes-128-ctr","ciphertext":"fc6607717861d1ec6bc77ed958315b57ea418a23fd5dc318697d98544f8b1f50","cipherparams":{"iv":"ba9d37b35e9330099288940415352484"},"kdf":"scrypt","kdfparams":{"dklen":32,"n":262144,"p":1,"r":8,"salt":"c69d7288501031a6fc21dc1c52f4a52c91f0ae25159f530a09f8dd5e7dd244fb"},"mac":"b7394fe7d97b8b73ee12e98515f353d7c47adaa8364c37dc23a4bf006c101f73"},"id":"f19e5d1f-a1a1-493e-9cce-c8cf86d8a061","version":3}`
 	pass    = "password123"
 	cfgPath = "../test/config.json"
 	// randomized name for testing
